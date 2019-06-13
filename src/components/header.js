44d3a26bbffd5fa380986aef20e76d61ab4eca99
Kenny Mark <geniounico@outlook.com>
2019-06-13 11:33:19 +0100

    removed config file from src

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./styles.css";
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
 
